c6691a241cb9e626025496153d7a330dacf7d026
Yuyuko1024 <dingwenxuan4@gmail.com>
2025-02-28 14:34:23 +0800

    ArtistApiFragment界面初步实现

--- app/src/main/java/net/hearnsoft/tcm/ui/fragments/apitest/ArtistApiFragment.java
+++ app/src/main/java/net/hearnsoft/tcm/ui/fragments/apitest/ArtistApiFragment.java
@@ -1,6 +1,34 @@
 package net.hearnsoft.tcm.ui.fragments.apitest;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import net.hearnsoft.tcm.databinding.FragmentArtistApiTestBinding;
+
 public class ArtistApiFragment extends Fragment {
+    private FragmentArtistApiTestBinding binding;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = FragmentArtistApiTestBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
 }

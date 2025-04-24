2de90ae5e7e6b3edad0d87160cd2ed8c1cd17534
Yuyuko1024 <dingwenxuan4@gmail.com>
2025-04-24 13:58:06 +0800

    Add xiaomi transparent status bar support

--- app/src/main/java/net/hearnsoft/tcm/ui/activity/BaseActivity.java
+++ app/src/main/java/net/hearnsoft/tcm/ui/activity/BaseActivity.java
@@ -1,10 +1,14 @@
 package net.hearnsoft.tcm.ui.activity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
@@ -12,5 +16,12 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
+        // 小米 沉浸式状态栏
+        if (Objects.equals(Build.BRAND, "Xiaomi") || Objects.equals(Build.BRAND, "Redmi")) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);       //设置沉浸式状态栏，在MIUI系统中，状态栏背景透明。原生系统中，状态栏背景半透明。
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);   //设置沉浸式虚拟键，在MIUI系统中，虚拟键背景透明。原生系统中，虚拟键背景半透明。
+        }
     }
 }

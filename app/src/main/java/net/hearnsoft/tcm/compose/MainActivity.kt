dee73a34a45a7fd81d277fee15b8eb766676687c
Yuyuko1024 <dingwenxuan4@gmail.com>
2025-08-08 14:11:21 +0800

    EdgeToEdge fix

--- app/src/main/java/net/hearnsoft/tcm/compose/MainActivity.kt
+++ app/src/main/java/net/hearnsoft/tcm/compose/MainActivity.kt
@@ -5,15 +5,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.OptIn
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.moriafly.salt.ui.UnstableSaltUiApi
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.UnstableApi
-import net.hearnsoft.tcm.compose.MainActivity
 import net.hearnsoft.tcm.compose.ui.theme.TouhouCloudMusicTheme
 import net.hearnsoft.tcm.compose.ui.views.AppRootView
 
@@ -25,6 +27,7 @@ import net.hearnsoft.tcm.compose.ui.views.AppRootView
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         enableEdgeToEdge()
         setContent {
             TouhouCloudMusicTheme {

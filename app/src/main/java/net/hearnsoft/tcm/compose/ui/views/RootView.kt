db230d047ebcac68c36e3eaf954e4b0046615da4
Yuyuko1024 <dingwenxuan4@gmail.com>
2025-09-07 21:54:43 +0800

    Set all page 500ms fade in fade out animate

--- app/src/main/java/net/hearnsoft/tcm/compose/ui/views/RootView.kt
+++ app/src/main/java/net/hearnsoft/tcm/compose/ui/views/RootView.kt
@@ -5,6 +5,9 @@ import android.app.Activity
 import androidx.activity.compose.BackHandler
 import androidx.annotation.OptIn
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.BoxWithConstraints
@@ -226,6 +229,18 @@ fun AppRootView(
                 NavHost(
                     navController = navController,
                     startDestination = ScreenRoute.Explore.route,
+                    enterTransition = {
+                        fadeIn(animationSpec = tween(500))
+                    },
+                    exitTransition = {
+                        fadeOut(animationSpec = tween(500))
+                    },
+                    popEnterTransition = {
+                        fadeIn(animationSpec = tween(500))
+                    },
+                    popExitTransition = {
+                        fadeOut(animationSpec = tween(500))
+                    },
                     modifier = Modifier
                         .nestedScroll(
                             topAppBarScrollBehavior.nestedScrollConnection

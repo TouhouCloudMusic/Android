cb4da010e1f36194566d2f295fa8d9360758da0d
Yuyuko1024 <dingwenxuan4@gmail.com>
2025-10-14 08:35:55 +0800

    Use dialog to show scan progress

--- app/src/main/java/net/hearnsoft/tcm/compose/ui/screens/MusicScanScreen.kt
+++ app/src/main/java/net/hearnsoft/tcm/compose/ui/screens/MusicScanScreen.kt
@@ -1,36 +1,31 @@
 package net.hearnsoft.tcm.compose.ui.screens
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.SnackbarDuration
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.media3.common.util.UnstableApi
 import com.moriafly.salt.ui.Item
 import com.moriafly.salt.ui.ItemArrowType
-import com.moriafly.salt.ui.ItemInfo
-import com.moriafly.salt.ui.ItemInfoType
+import com.moriafly.salt.ui.ItemOuterTip
 import com.moriafly.salt.ui.RoundedColumn
-import com.moriafly.salt.ui.Text
+import com.moriafly.salt.ui.TextButton
 import com.moriafly.salt.ui.UnstableSaltUiApi
+import com.moriafly.salt.ui.dialog.BasicDialog
+import com.moriafly.salt.ui.dialog.DialogTitle
+import com.moriafly.salt.ui.outerPadding
 import kotlinx.coroutines.delay
 import net.hearnsoft.tcm.compose.R
 import net.hearnsoft.tcm.compose.ui.viewmodel.PlayerViewModel
@@ -56,10 +51,32 @@ fun MusicScanScreen(
     }
 
     Column(modifier.fillMaxSize()) {
+        var showDialog by remember { mutableStateOf(false) }
+
+        if (showDialog) {
+            ScanDialog(
+                title = "正在扫描音乐",
+                content = if (scanProgress == null && !scanCompleted) {
+                    "正在准备扫描音乐库，请稍候..."
+                } else if (scanProgress != null) {
+                    scanProgress!!
+                } else run {
+                    "扫描完成！"
+                },
+                confirmText = "确定",
+                onDismissRequest = { showDialog = false },
+                properties = DialogProperties(
+                    dismissOnClickOutside = false
+                )
+            )
+        }
+
+
         RoundedColumn {
             Item(
                 text = "扫描音乐",
                 onClick = {
+                    showDialog = true
                     playerViewModel.scanAndUpdateMusicLibrary()
                 },
                 arrowType = ItemArrowType.None,
@@ -68,40 +85,34 @@ fun MusicScanScreen(
             )
         }
 
-        AnimatedVisibility(
-            visible = scanProgress != null
-        ) {
-            RoundedColumn {
-                Row(
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    CircularProgressIndicator(
-                        modifier = Modifier.size(16.dp)
-                    )
-                    Text(
-                        text = scanProgress ?: "正在处理...",
-                        modifier = Modifier.padding(start = 12.dp),
-                        maxLines = 2,
-                        overflow = TextOverflow.Ellipsis
-                    )
-                }
-            }
-        }
-
-        AnimatedVisibility(
-            visible = scanCompleted
-        ) {
-            RoundedColumn {
-                ItemInfo(
-                    text = "扫描完成",
-                    infoType = ItemInfoType.Success
-                )
-            }
-        }
-
     }
 
+}
+
+@Composable
+private fun ScanDialog(
+    onDismissRequest: () -> Unit,
+    properties: DialogProperties = DialogProperties(),
+    title: String,
+    content: String,
+    confirmText: String,
+    dialogButtonEnabled: Boolean = true
+) {
+    BasicDialog(
+        onDismissRequest = onDismissRequest,
+        properties = properties
+    ) {
+        DialogTitle(text = title)
+        ItemOuterTip(text = content)
+        TextButton(
+            enabled = dialogButtonEnabled,
+            onClick = {
+                onDismissRequest()
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .outerPadding(),
+            text = confirmText
+        )
+    }
 }
